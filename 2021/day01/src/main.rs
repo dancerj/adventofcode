cf36b9a49e29011bf9f4d30c5bc156109759f79d
Junichi Uekawa <dancer@debian.org>
2021-12-19 11:28:18 +0900

    part 2

--- 2021/day01/src/main.rs
+++ 2021/day01/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn main() {
+fn part1() {
     let file = File::open("input.txt").unwrap();
     let mut prev = None;
     let mut count = 0;
@@ -24,4 +24,38 @@ fn main() {
     }
     println!("{}", count);
     println!("{}", increased_count);
+}
+
+fn part2() {
+    let file = File::open("input.txt").unwrap();
+    let input_lines: Vec<u64> = io::BufReader::new(file)
+        .lines()
+        .map(|x| x.unwrap().parse::<u64>().unwrap())
+        .collect();
+    let mut prev = None;
+    let mut count = 0;
+    let mut increased_count = 0;
+    for i in 2..input_lines.len() {
+        let avg = (input_lines[i] + input_lines[i - 1] + input_lines[i - 2]) as f64 / 3f64;
+        match prev {
+            None => {
+                // ignore
+            }
+            Some(prev_num) => {
+                if avg > prev_num {
+                    // increased
+                    increased_count += 1;
+                }
+            }
+        }
+        prev = Some(avg);
+        count += 1;
+    }
+    println!("{}", count);
+    println!("{}", increased_count);
+}
+
+fn main() {
+    part1();
+    part2();
 }
