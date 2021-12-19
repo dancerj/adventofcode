e750a6f4e81713d1a8b2db934ca7bffa072ad74f
Junichi Uekawa <dancer@debian.org>
2021-12-19 21:42:41 +0900

    day02 part2

--- 2021/day02/src/main.rs
+++ 2021/day02/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn process(s: &str) -> (u64, u64) {
+fn process1(s: &str) -> (u64, u64) {
     let mut depth = 0;
     let mut horizontal = 0;
 
@@ -31,12 +31,47 @@ fn process(s: &str) -> (u64, u64) {
 
 fn part1() {
     let text = fs::read_to_string("input.txt").unwrap();
-    let (depth, horizontal) = process(&text);
+    let (depth, horizontal) = process1(&text);
+    println!("{} {} {}", depth, horizontal, depth * horizontal);
+}
+
+fn process2(s: &str) -> (u64, u64) {
+    let mut depth = 0;
+    let mut horizontal = 0;
+    let mut aim = 0;
+
+    for line in s.lines() {
+        let mut v = line.split(' ');
+        let command = v.next().unwrap();
+        let value = v.next().unwrap().parse::<u64>().unwrap();
+        match command {
+            "forward" => {
+                horizontal += value;
+                depth += aim * value;
+            }
+            "down" => {
+                aim += value;
+            }
+            "up" => {
+                aim -= value;
+            }
+            _ => {
+                panic!("Hello world");
+            }
+        }
+    }
+    (depth, horizontal)
+}
+
+fn part2() {
+    let text = fs::read_to_string("input.txt").unwrap();
+    let (depth, horizontal) = process2(&text);
     println!("{} {} {}", depth, horizontal, depth * horizontal);
 }
 
 fn main() {
     part1();
+    part2();
 }
 
 #[cfg(test)]
@@ -44,16 +79,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_hoge() -> Result<(), std::io::Error> {
+    fn test_part1() -> Result<(), std::io::Error> {
         let commands = "forward 5
 down 5
 forward 8
 up 3
 down 8
 forward 2";
-        let (depth, horizontal) = process(commands);
+        let (depth, horizontal) = process1(commands);
         // println!("{:?}", result);
         assert_eq!(depth, 10);
+        assert_eq!(horizontal, 15);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<(), std::io::Error> {
+        let commands = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+        let (depth, horizontal) = process2(commands);
+        assert_eq!(depth, 60);
         assert_eq!(horizontal, 15);
         Ok(())
     }
