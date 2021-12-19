160d66278b4ff2b6b1609256002483e5a3e71dc7
Junichi Uekawa <dancer@debian.org>
2021-12-19 21:38:21 +0900

    day02 part1

---
+++ 2021/day02/src/main.rs
@@ -0,0 +1,60 @@
+use std::fs;
+
+fn process(s: &str) -> (u64, u64) {
+    let mut depth = 0;
+    let mut horizontal = 0;
+
+    for line in s.lines() {
+        let mut v = line.split(' ');
+        let command = v.next().unwrap();
+        let value = v.next().unwrap().parse::<u64>().unwrap();
+        match command {
+            "forward" => {
+                horizontal += value;
+            }
+            "down" => {
+                depth += value;
+            }
+            "up" => {
+                depth -= value;
+            }
+            "back" => {
+                horizontal -= value;
+            }
+            _ => {
+                panic!("Hello world");
+            }
+        }
+    }
+    (depth, horizontal)
+}
+
+fn part1() {
+    let text = fs::read_to_string("input.txt").unwrap();
+    let (depth, horizontal) = process(&text);
+    println!("{} {} {}", depth, horizontal, depth * horizontal);
+}
+
+fn main() {
+    part1();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hoge() -> Result<(), std::io::Error> {
+        let commands = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+        let (depth, horizontal) = process(commands);
+        // println!("{:?}", result);
+        assert_eq!(depth, 10);
+        assert_eq!(horizontal, 15);
+        Ok(())
+    }
+}
