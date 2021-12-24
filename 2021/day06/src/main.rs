4c17cce0ced742c59060186f694f6a0648cc2bb9
Junichi Uekawa <dancer@debian.org>
2021-12-24 12:27:43 +0900

    Speed up but the small input still causes large memory usage.
    approx 40GB memory.
    255 26984457539 22.560387908s
    26984457539

--- 2021/day06/src/main.rs
+++ 2021/day06/src/main.rs
@@ -1,5 +1,12 @@
-fn parse_str(s: &str) -> Vec<u32> {
-    s.split(",").map(|x| /*dbg!*/(x).trim().parse().unwrap()).collect()
+use std::fmt::Debug;
+use std::str::FromStr;
+use std::time::Instant;
+
+fn parse_str<T: FromStr>(s: &str) -> Vec<T>
+where
+    <T as FromStr>::Err: Debug,
+{
+    s.split(",").map(|x| x.trim().parse().unwrap()).collect()
 }
 
 fn iterate(input: &Vec<u32>) -> Vec<u32> {
@@ -10,16 +17,40 @@ fn iterate(input: &Vec<u32>) -> Vec<u32> {
         .collect()
 }
 
-fn iterate_days(days: u32, input: &Vec<u32>) -> Vec<u32>{
-    let mut next : Vec<u32> = input.to_vec();
-    (0..days).for_each(|_day| {
+fn iterate_days(days: u32, input: &Vec<u32>) -> Vec<u32> {
+    let mut next: Vec<u32> = input.to_vec();
+    (0..days).for_each(|day| {
         // println!("{} {:?}", day, next);
+        println!("{} {}", day, next.len());
         next = iterate(&next);
     });
     next.clone()
 }
 
-fn main() {}
+fn less_allocation_iterate(days: u16, input: &Vec<u8>) -> Vec<u8> {
+    let mut current: Vec<u8> = input.to_vec();
+    (0..days).for_each(|day| {
+        let now = Instant::now();
+        let mut to_push = vec![];
+        (0..current.len()).for_each(|n| {
+            if current[n] == 0 {
+                current[n] = 6;
+                to_push.push(8);
+            } else {
+                current[n] -= 1;
+            }
+        });
+        current.extend_from_slice(&to_push);
+        let elapsed_time = now.elapsed();
+        println!("{} {} {:?}", day, current.len(), elapsed_time);
+    });
+    current
+}
+
+fn main() {
+    let input = vec![3, 4, 3, 1, 2];
+    println!("{}", less_allocation_iterate(256, &input).len());
+}
 
 #[cfg(test)]
 mod tests {
@@ -29,21 +60,28 @@ mod tests {
 
     #[test]
     fn test_parse_str() {
-        assert_eq!(parse_str(COMMANDS), [3, 4, 3, 1, 2]);
+        assert_eq!(parse_str::<u32>(COMMANDS), [3, 4, 3, 1, 2]);
     }
 
     #[test]
     fn test_iterate_days_short() {
-        let input= parse_str(COMMANDS);
+        let input = parse_str::<u32>(COMMANDS);
         assert_eq!(iterate_days(18, &input).len(), 26);
         assert_eq!(iterate_days(80, &input).len(), 5934);
     }
 
     #[test]
     fn test_iterate_days_real() {
-        let input= parse_str(include_str!("input.txt"));
+        let input = parse_str::<u32>(include_str!("input.txt"));
         assert_eq!(iterate_days(18, &input).len(), 1565);
         assert_eq!(iterate_days(80, &input).len(), 352195);
     }
 
+    #[test]
+    fn test_iterate_days_less_allocation() {
+        let input = parse_str::<u8>(COMMANDS);
+        assert_eq!(less_allocation_iterate(18, &input).len(), 26);
+        // assert_eq!(less_allocation_iterate(80, &input).len(), 5934);
+        // assert_eq!(less_allocation_iterate(256, &input).len(), 5934);
+    }
 }
