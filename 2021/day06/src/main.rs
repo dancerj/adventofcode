9bc3c9c864beaf610187ec54ebc926dc429a3c68
Junichi Uekawa <dancer@debian.org>
2021-12-24 11:55:24 +0900

    day 6 part 1

---
+++ 2021/day06/src/main.rs
@@ -0,0 +1,41 @@
+fn parse_str(s: &str) -> Vec<u32> {
+    s.split(",").map(|x| x.parse().unwrap()).collect()
+}
+
+fn iterate(input: &Vec<u32>) -> Vec<u32> {
+    input
+        .iter()
+        .map(|&x| if x == 0 { vec![6, 8] } else { vec![x - 1] })
+        .flatten()
+        .collect()
+}
+
+fn iterate_days(days: u32, input: &Vec<u32>) -> Vec<u32>{
+    let mut next : Vec<u32> = input.to_vec();
+    (0..days).for_each(|day| {
+        println!("{} {:?}", day, next);
+        next = iterate(&next);
+    });
+    next.clone()
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const COMMANDS: &str = "3,4,3,1,2";
+
+    #[test]
+    fn test_parse_str() {
+        assert_eq!(parse_str(COMMANDS), [3, 4, 3, 1, 2]);
+    }
+
+    #[test]
+    fn test_iterate_days_18() {
+        let input= parse_str(COMMANDS);
+        assert_eq!(iterate_days(18, &input).len(), 26);
+        assert_eq!(iterate_days(80, &input).len(), 5934);
+    }
+}
