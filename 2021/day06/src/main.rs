8725f3d15bcddab5d0b8faaa9e5e8bd190a536ed
Junichi Uekawa <dancer@debian.org>
2021-12-24 12:00:50 +0900

    day 06 part 1

--- 2021/day06/src/main.rs
+++ 2021/day06/src/main.rs
@@ -1,5 +1,5 @@
 fn parse_str(s: &str) -> Vec<u32> {
-    s.split(",").map(|x| x.parse().unwrap()).collect()
+    s.split(",").map(|x| /*dbg!*/(x).trim().parse().unwrap()).collect()
 }
 
 fn iterate(input: &Vec<u32>) -> Vec<u32> {
@@ -12,8 +12,8 @@ fn iterate(input: &Vec<u32>) -> Vec<u32> {
 
 fn iterate_days(days: u32, input: &Vec<u32>) -> Vec<u32>{
     let mut next : Vec<u32> = input.to_vec();
-    (0..days).for_each(|day| {
-        println!("{} {:?}", day, next);
+    (0..days).for_each(|_day| {
+        // println!("{} {:?}", day, next);
         next = iterate(&next);
     });
     next.clone()
@@ -33,9 +33,17 @@ mod tests {
     }
 
     #[test]
-    fn test_iterate_days_18() {
+    fn test_iterate_days_short() {
         let input= parse_str(COMMANDS);
         assert_eq!(iterate_days(18, &input).len(), 26);
         assert_eq!(iterate_days(80, &input).len(), 5934);
     }
+
+    #[test]
+    fn test_iterate_days_real() {
+        let input= parse_str(include_str!("input.txt"));
+        assert_eq!(iterate_days(18, &input).len(), 1565);
+        assert_eq!(iterate_days(80, &input).len(), 352195);
+    }
+
 }
