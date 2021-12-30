e97e5f1b42c4e415628cb65b570894459b7bdec1
Junichi Uekawa <dancer@debian.org>
2021-12-30 16:14:27 +0900

    day10 part2

--- 2021/day10/src/main.rs
+++ 2021/day10/src/main.rs
@@ -33,6 +33,48 @@ fn find_invalid(s: &str) -> u32 {
     score
 }
 
+fn completion_score(s: &str) -> u64 {
+    let k_scores: HashMap<char, u64> = [(')', 1), (']', 2), ('}', 3), ('>', 4)]
+        .iter()
+        .cloned()
+        .collect();
+
+    let k_match: HashMap<char, char> = [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]
+        .iter()
+        .cloned()
+        .collect();
+
+    let mut result = s
+        .lines()
+        .filter_map(|line| {
+            let mut stack: Vec<char> = vec![];
+            for c in line.chars() {
+                if k_match.contains_key(&c) {
+                    stack.push(c);
+                } else {
+                    let top = stack.pop().unwrap();
+                    if k_match.get(&top).unwrap() != &c {
+                        // invalid closing brace, skip the rest of the line.
+                        println!("invalid skip");
+                        return None;
+                    }
+                }
+            }
+            let mut score: u64 = 0;
+            stack.iter().rev().for_each(|c| {
+                score *= 5;
+                score += k_scores.get(k_match.get(&c).unwrap()).unwrap();
+            });
+            dbg!(score);
+            Some(score)
+        })
+        .collect::<Vec<u64>>();
+    result.sort();
+    dbg!(&result);
+    assert_eq!(result.len() % 2, 1);
+    result[result.len() / 2]
+}
+
 fn main() {}
 
 #[cfg(test)]
@@ -60,5 +102,17 @@ mod tests {
     fn test_find_invalid_real() {
         let i = find_invalid(include_str!("input.txt"));
         assert_eq!(i, 415953);
+    }
+
+    #[test]
+    fn test_completion_score() {
+        let i = completion_score(COMMANDS);
+        assert_eq!(i, 288957);
+    }
+
+    #[test]
+    fn test_completion_score_real() {
+        let i = completion_score(include_str!("input.txt"));
+        assert_eq!(i, 2292863731);
     }
 }
