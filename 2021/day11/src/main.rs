44a041b5b5ab3379db35ea7d62688c3cbde8e4e1
Junichi Uekawa <dancer@debian.org>
2022-01-03 12:20:20 +0900

    start doing iteration.

--- 2021/day11/src/main.rs
+++ 2021/day11/src/main.rs
@@ -11,6 +11,51 @@ fn parse(s: &str) -> Vec<Vec<u32>> {
         .collect()
 }
 
+struct Context {
+    map: Vec<Vec<u32>>,
+    xsize: usize,
+    ysize: usize,
+}
+
+impl Context {
+    fn new(map: Vec<Vec<u32>>) -> Self {
+        let mut ret = Self {
+            map,
+            xsize: 0,
+            ysize: 0,
+        };
+        ret.xsize = ret.map[0].len();
+        ret.ysize = ret.map.len();
+        ret
+    }
+    fn adjust(&mut self, x: i32, y: i32) {
+        if x >= 0 && y >= 0 && x < self.xsize as i32 && y < self.ysize as i32 {
+            self.map[y as usize][x as usize] += 1;
+            if self.map[y as usize][x as usize] == 10 {
+                // flash
+                self.adjust(x - 1, y - 1);
+                self.adjust(x - 1, y);
+                self.adjust(x - 1, y + 1);
+                self.adjust(x, y - 1);
+                self.adjust(x, y + 2);
+                self.adjust(x + 1, y - 1);
+                self.adjust(x + 1, y);
+                self.adjust(x + 1, y + 1);
+            }
+        }
+    }
+}
+
+fn count_flashes(s: &str) -> u32 {
+    let mut c = Context::new(parse(s));
+
+    for y in 0..c.ysize {
+        for x in 0..c.xsize {
+            c.adjust(x as i32, y as i32);
+        }
+    }
+    0
+}
 fn main() {}
 
 #[cfg(test)]
@@ -32,5 +77,11 @@ mod tests {
     fn test_parse() {
         let i = parse(COMMANDS);
         assert_eq!(i[0][0], 5);
+    }
+
+    #[test]
+    fn test_iterate() {
+        let i = count_flashes(COMMANDS);
+        assert_eq!(i, 5);
     }
 }
