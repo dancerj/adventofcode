a5d36e471026a97e86b9c18e8f375d6a3cc46474
Junichi Uekawa <dancer@debian.org>
2022-01-09 11:47:59 +0900

    more progress.

--- 2021/day13/src/main.rs
+++ 2021/day13/src/main.rs
@@ -1,7 +1,7 @@
 //use std::collections::HashMap;
 //use std::collections::HashSet;
 
-fn parse(s: &str) -> (Vec<(u32, u32)>, Vec<&str>) {
+fn parse(s: &str) -> (Vec<(u32, u32)>, Vec<(&str, u32)>) {
     let mut lines = s.lines();
     let positions: Vec<(u32, u32)> = lines
         .by_ref()
@@ -17,7 +17,39 @@ fn parse(s: &str) -> (Vec<(u32, u32)>, Vec<&str>) {
             }
         })
         .collect();
-    let fold_commands: Vec<&str> = lines.by_ref().collect();
+    let size = positions.iter().map(|(x, y)| x.max(y)).max();
+    // let mut matrix = vec![vec![]];
+
+    let fold_commands: Vec<_> = lines
+        .map(|line| {
+            let mut words = line.split(" ");
+            match (
+                words.next().unwrap(),
+                words.next().unwrap(),
+                words.next().unwrap(),
+            ) {
+                ("fold", "along", command) => {
+                    let mut words = command.split("=");
+                    match (words.next().unwrap(), words.next().unwrap()) {
+                        ("x", value) => {
+                            let value = value.parse().unwrap();
+                            ("x", value)
+                        }
+                        ("y", value) => {
+                            let value = value.parse().unwrap();
+                            ("y", value)
+                        }
+                        _ => {
+                            panic!();
+                        }
+                    }
+                }
+                _ => {
+                    panic!();
+                }
+            }
+        })
+        .collect();
     (positions, fold_commands)
 }
 fn main() {}
@@ -51,6 +83,6 @@ fold along x=5";
     fn test_parse() {
         let i = parse(COMMANDS);
         assert_eq!(i.0[0], (6, 10));
-        assert_eq!(i.1[0], "fold along y=7");
+        assert_eq!(i.1[0], ("y", 7));
     }
 }
