ec8c5c60eecabc34ce53cdff4d305a6257fbb5f7
Junichi Uekawa <dancer@debian.org>
2022-01-09 11:28:53 +0900

    parse int.

--- 2021/day13/src/main.rs
+++ 2021/day13/src/main.rs
@@ -1,9 +1,22 @@
 //use std::collections::HashMap;
 //use std::collections::HashSet;
 
-fn parse(s: &str) -> (Vec<&str>, Vec<&str>) {
+fn parse(s: &str) -> (Vec<(u32,u32)>, Vec<&str>) {
     let mut lines = s.lines();
-    let positions: Vec<&str> = lines.by_ref().take_while(|&line| line != "").collect();
+    let positions: Vec<(u32, u32)> = lines
+        .by_ref()
+        .map_while(|line| {
+            if line != "" {
+                let mut words = line.split(",");
+                Some((
+                    words.next().unwrap().parse().unwrap(),
+                    words.next().unwrap().parse().unwrap(),
+                ))
+            } else {
+                None
+            }
+        })
+        .collect();
     let fold_commands: Vec<&str> = lines.by_ref().collect();
     (positions, fold_commands)
 }
@@ -37,7 +50,7 @@ fold along x=5";
     #[test]
     fn test_parse() {
         let i = parse(COMMANDS);
-        assert_eq!(i.0[0], "6,10");
+        assert_eq!(i.0[0], (6,10));
         assert_eq!(i.1[0], "fold along y=7");
     }
 }
