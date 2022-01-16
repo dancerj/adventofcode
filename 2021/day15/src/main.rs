d98e43a1d1a3f8ea100caa6c76f77d7bbf0cc73e
Junichi Uekawa <dancer@debian.org>
2022-01-16 17:27:02 +0900

    overflows stack

--- 2021/day15/src/main.rs
+++ 2021/day15/src/main.rs
@@ -1,5 +1,6 @@
 struct MapInfo {
     map: Vec<Vec<u32>>,
+    tentative_cost: Vec<Vec<u32>>,
     width: usize,
     height: usize,
 }
@@ -12,67 +13,59 @@ impl MapInfo {
             .collect();
         let width = map[0].len();
         let height = map.len();
-        MapInfo { map, width, height }
+        let tentative_cost = vec![vec![u32::MAX; width]; height];
+        MapInfo {
+            map,
+            tentative_cost,
+            width,
+            height,
+        }
     }
 
     fn cost(&self, at: &(usize, usize)) -> u32 {
         self.map[at.1][at.0]
     }
-}
 
-fn shortest_path(
-    map: &MapInfo,
-    route: &Vec<(usize, usize)>,
-    position: (Option<usize>, Option<usize>),
-    cost: u32,
-) -> Option<u32> {
-    if position.0 == None
-        || position.1 == None
-        || position.0.unwrap() >= map.width
-        || position.1.unwrap() >= map.height
-    {
-        return None;
-    }
-    let position = (position.0.unwrap(), position.1.unwrap());
+    fn shortest_path(
+        &mut self,
+        position: (Option<usize>, Option<usize>),
+        cost: u32,
+    ) -> Option<u32> {
+        if position.0 == None
+            || position.1 == None
+            || position.0.unwrap() >= self.width
+            || position.1.unwrap() >= self.height
+        {
+            return None;
+        }
+        let position = (position.0.unwrap(), position.1.unwrap());
 
-    // Do not come back.
-    if route.iter().any(|&x| x == position) {
-        return None;
-    }
-    let mut route = route.clone();
-    route.push(position);
+        let cost = cost + self.cost(&position);
 
-    // Do not try too much
-    if cost > 41 || route.len() > 20 {
-        return None;
-    }
-    let cost = cost + map.cost(&position);
-    if position == (map.width - 1, map.height - 1) {
-        // Goal!
-        return Some(cost);
-    }
+        if cost < self.tentative_cost[position.1][position.0] {
+            self.tentative_cost[position.1][position.0] = cost;
+        } else {
+            // There was already someone who visited this node with lower cost.
+            return None;
+        }
 
-    // Iterate all others and find minimal route.
-    let cost = vec![
-        shortest_path(
-            map,
-            &route,
-            (position.0.checked_sub(1), Some(position.1)),
-            cost,
-        ),
-        shortest_path(
-            map,
-            &route,
-            (Some(position.0), position.1.checked_sub(1)),
-            cost,
-        ),
-        shortest_path(map, &route, (Some(position.0 + 1), Some(position.1)), cost),
-        shortest_path(map, &route, (Some(position.0), Some(position.1 + 1)), cost),
-    ]
-    .iter()
-    .filter_map(|&x| x)
-    .min();
-    cost
+        if position == (self.width - 1, self.height - 1) {
+            // Goal!
+            return Some(cost);
+        }
+
+        // Iterate all others and find minimal route.
+        let cost = vec![
+            self.shortest_path((position.0.checked_sub(1), Some(position.1)), cost),
+            self.shortest_path((Some(position.0), position.1.checked_sub(1)), cost),
+            self.shortest_path((Some(position.0 + 1), Some(position.1)), cost),
+            self.shortest_path((Some(position.0), Some(position.1 + 1)), cost),
+        ]
+        .iter()
+        .filter_map(|&x| x)
+        .min();
+        cost
+    }
 }
 
 fn main() {}
@@ -94,9 +87,19 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        let t = MapInfo::new(COMMANDS);
+        let mut t = MapInfo::new(COMMANDS);
         assert_eq!(t.map[0][0], 1);
-        let cost = shortest_path(&t, &vec![], (Some(0), Some(0)), 0).unwrap() - 1;
+        let cost = t.shortest_path((Some(0), Some(0)), 0).unwrap() - 1;
         assert_eq!(cost, 40);
+    }
+
+    #[test]
+    fn test_parse_real() {
+        let mut t = MapInfo::new(include_str!("input.txt"));
+        assert_eq!(t.width, 100);
+        assert_eq!(t.height, 100);
+        assert_eq!(t.map[0][0], 3);
+        // let cost = t.shortest_path((Some(0), Some(0)), 0).unwrap() - 1;
+        // assert_eq!(cost, 40);
     }
 }
