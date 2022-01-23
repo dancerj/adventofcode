2f236c14c398f903fa79b32ded40a165d71e2be6
Junichi Uekawa <dancer@debian.org>
2022-01-23 18:24:42 +0900

    debugging.

--- 2021/day16/src/main.rs
+++ 2021/day16/src/main.rs
@@ -76,6 +76,78 @@ impl BitReader {
         version
     }
 
+    fn parse_operators(&mut self) -> u32 {
+        let mut values: Vec<u32> = vec![];
+        let _version = self.read(3);
+        let t = self.read(3);
+        if t == PacketType::Literal as u32 {
+            values.push(self.read_literal());
+            println!("literal {:?}", values);
+        } else {
+            match self.read(1) {
+                0 => {
+                    // length of packet
+                    let l = self.read(15);
+                    println!("component of len {}", l);
+                    let start = self.offset;
+                    while (self.offset - start) < l.try_into().unwrap() {
+                        values.push(self.parse_operators());
+                    }
+                }
+                1 => {
+                    // number of sub-packets
+                    let n = self.read(11);
+
+                    println!("component of {} sub packets", n);
+                    for _ in 0..n {
+                        values.push(self.parse_operators());
+                    }
+                }
+                _ => {
+                    panic!()
+                }
+            };
+        }
+
+        let t: PacketType = unsafe { std::mem::transmute(t as i8) };
+
+        match t {
+            PacketType::Sum => values.iter().sum::<u32>(),
+            PacketType::Product => {
+                println!("product {:?}", values);
+                values.iter().product::<u32>()
+            },
+            PacketType::Minimum => *values.iter().min().unwrap(),
+            PacketType::Maximum => *values.iter().max().unwrap(),
+            PacketType::Literal => {
+                assert_eq!(values.len(), 1);
+                values[0]
+            }
+            PacketType::GreaterThan => {
+                assert_eq!(values.len(), 2);
+                if values[0] > values[1] {
+                    1
+                } else {
+                    0
+                }
+            }
+            PacketType::LessThan => {
+                if values[0] < values[1] {
+                    1
+                } else {
+                    0
+                }
+            }
+            PacketType::EqualTo => {
+                if values[0] == values[1] {
+                    1
+                } else {
+                    0
+                }
+            }
+        }
+    }
+
     fn sync_to_alignment(&mut self) {
         // Hex values are 8 bit aligned.
         if self.offset % 8 != 0 {
@@ -107,10 +179,17 @@ fn parse(s: &str) -> BitReader {
 
 fn main() {}
 
+#[allow(dead_code)] // I never construct these, they are converted from int.
+#[repr(i8)]
 enum PacketType {
+    Sum = 0,
+    Product = 1,
+    Minimum = 2,
+    Maximum = 3,
     Literal = 4,
-    OperatorA = 6,
-    OperatorB = 3,
+    GreaterThan = 5,
+    LessThan = 6,
+    EqualTo = 7,
 }
 
 #[cfg(test)]
@@ -130,7 +209,7 @@ mod tests {
 
         // 38006F45291200
         assert_eq!(t.read(3), 1);
-        assert_eq!(t.read(3), PacketType::OperatorA as u32);
+        assert_eq!(t.read(3), PacketType::LessThan as u32);
         assert_eq!(t.read(1), 0); // length type ID.
         assert_eq!(t.read(15), 27); // length.
         t.read(27); // Skip.
@@ -138,7 +217,7 @@ mod tests {
 
         // EE00D40C823060
         assert_eq!(t.read(3), 7);
-        assert_eq!(t.read(3), PacketType::OperatorB as u32);
+        assert_eq!(t.read(3), PacketType::Maximum as u32);
         assert_eq!(t.read(1), 1); // length type ID.
         assert_eq!(t.read(11), 3); // number of sub-packets
     }
@@ -171,5 +250,17 @@ mod tests {
             parse(include_str!("input.txt")).add_version_numbers_internal(),
             940
         );
+    }
+
+    #[test]
+    fn test_parse_operators() {
+        assert_eq!(parse("C200B40A82").parse_operators(), 3);
+        assert_eq!(parse("04005AC33890").parse_operators(), 54);
+        assert_eq!(parse("880086C3E88112").parse_operators(), 7);
+        assert_eq!(parse("CE00C43D881120").parse_operators(), 9);
+        assert_eq!(parse("D8005AC2A8F0").parse_operators(), 1);
+        assert_eq!(parse("F600BC2D8F").parse_operators(), 0);
+        assert_eq!(parse("9C005AC2F8F0").parse_operators(), 0);
+        assert_eq!(parse("9C0141080250320F1802104A08").parse_operators(), 1);
     }
 }
