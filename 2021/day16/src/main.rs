5695bc38d64bb61b05321b2232315714f08a9747
Junichi Uekawa <dancer@debian.org>
2022-01-23 17:11:41 +0900

    practice doing parsing

--- 2021/day16/src/main.rs
+++ 2021/day16/src/main.rs
@@ -1,18 +1,81 @@
-fn parse(s: &str) -> String {
+struct BitReader {
+    data: String,
+    offset: usize,
+}
+
+impl BitReader {
+    fn new(s: &str) -> Self {
+        Self {
+            data: s.to_string(),
+            offset: 0,
+        }
+    }
+
+    fn read(&mut self, n: usize) -> u32 {
+        let result = (0..n)
+            .map(|shift| {
+                if self.data.chars().nth(self.offset + shift) == Some('1') {
+                    1u32 << (n - shift - 1)
+                } else {
+                    0
+                }
+            })
+            .sum();
+        self.offset += n;
+        result
+    }
+
+    fn read_literal(&mut self) -> u32 {
+        // prerequisite is that previous reads were 6, 4 (literal
+        // value).
+        let mut result = 0;
+        loop {
+            let cont = self.read(1) > 0;
+            let value = self.read(4);
+            result <<= 4;
+            result += value;
+            if !cont {
+                break;
+            }
+        }
+        result
+    }
+
+    fn sync_to_alignment(&mut self) {
+        // Hex values are 8 bit aligned.
+        if self.offset % 8 != 0 {
+            assert!(self.read(8 - self.offset % 8) == 0)
+        }
+    }
+}
+
+fn parse(s: &str) -> BitReader {
     let b: String = s
         .chars()
         .map(|hex| {
             let num = hex.to_digit(16).unwrap();
-            let binary: String = (0..3)
-                .map(|shift| if num & (1u32 << shift) > 0 { "1" } else { "0" })
+            let binary: String = (0..4)
+                .map(|shift| {
+                    if num & (1u32 << (4 - shift - 1)) > 0 {
+                        "1"
+                    } else {
+                        "0"
+                    }
+                })
                 .collect();
             binary
         })
         .collect();
-    b
+    BitReader::new(&b)
 }
 
 fn main() {}
+
+enum PacketType {
+    Literal = 4,
+    OperatorA = 6,
+    OperatorB = 3,
+}
 
 #[cfg(test)]
 mod tests {
@@ -22,7 +85,25 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        let t = parse(COMMANDS);
-        // panic!(t);
+        let mut t = parse(COMMANDS);
+        // D2FE28
+        assert_eq!(t.read(3), 6);
+        assert_eq!(t.read(3), PacketType::Literal as u32);
+        assert_eq!(t.read_literal(), 2021);
+        t.sync_to_alignment();
+
+        // 38006F45291200
+        assert_eq!(t.read(3), 1);
+        assert_eq!(t.read(3), PacketType::OperatorA as u32);
+        assert_eq!(t.read(1), 0); // length type ID.
+        assert_eq!(t.read(15), 27); // length.
+        t.read(27); // Skip.
+        t.sync_to_alignment();
+
+        // EE00D40C823060
+        assert_eq!(t.read(3), 7);
+        assert_eq!(t.read(3), PacketType::OperatorB as u32);
+        assert_eq!(t.read(1), 1); // length type ID.
+        assert_eq!(t.read(11), 3); // number of sub-packets
     }
 }
