73ebecc8c465828bd8179314b917630ae3e34db7
Junichi Uekawa <dancer@debian.org>
2022-01-23 17:31:54 +0900

    initial storyline.

--- 2021/day16/src/main.rs
+++ 2021/day16/src/main.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 struct BitReader {
     data: String,
     offset: usize,
@@ -39,6 +41,39 @@ impl BitReader {
             }
         }
         result
+    }
+
+    fn add_version_numbers_internal(&mut self) -> u32 {
+        let mut version = self.read(3);
+        let t = self.read(3);
+        if t == PacketType::Literal as u32 {
+            println!("literal {}", version);
+            self.read_literal();
+        } else {
+            match self.read(1) {
+                0 => {
+                    // length of packet
+                    let l = self.read(15);
+                    println!("component of len {}, version {}", l, version);
+                    let start = self.offset;
+                    while (self.offset - start) < l.try_into().unwrap() {
+                        version += self.add_version_numbers_internal();
+                    }
+                }
+                1 => {
+                    // number of sub-packets
+                    let n = self.read(11);
+                    println!("component of {} sub packets, version {} ", n, version);
+                    for _ in 0..n {
+                        version += self.add_version_numbers_internal();
+                    }
+                }
+                _ => {
+                    panic!()
+                }
+            };
+        };
+        version
     }
 
     fn sync_to_alignment(&mut self) {
@@ -105,5 +140,27 @@ mod tests {
         assert_eq!(t.read(3), PacketType::OperatorB as u32);
         assert_eq!(t.read(1), 1); // length type ID.
         assert_eq!(t.read(11), 3); // number of sub-packets
+    }
+
+    #[test]
+    fn test_add_version_numbers_internal() {
+        let mut t = parse(COMMANDS);
+        assert_eq!(t.add_version_numbers_internal(), 6);
+        assert_eq!(
+            parse("8A004A801A8002F478").add_version_numbers_internal(),
+            16
+        );
+        assert_eq!(
+            parse("620080001611562C8802118E34").add_version_numbers_internal(),
+            12
+        );
+        assert_eq!(
+            parse("C0015000016115A2E0802F182340").add_version_numbers_internal(),
+            23
+        );
+        assert_eq!(
+            parse("A0016C880162017C3686B18A3D4780").add_version_numbers_internal(),
+            31
+        );
     }
 }
