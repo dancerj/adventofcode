fa8625b76e59aaca0c68e87cec154138ef4d1073
Junichi Uekawa <dancer@debian.org>
2025-01-03 09:56:09 +0900

    complete part2 of day 1

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,5 +1,7 @@
-fn parse_input_string(input: &str) -> i32 {
-    let (mut v1, mut v2): (Vec<i32>, Vec<i32>) = input
+use std::collections::BTreeMap;
+
+fn parse_string(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let (v1, v2): (Vec<i32>, Vec<i32>) = input
         .lines()
         .map(|line| {
             let mut words = line.split_ascii_whitespace();
@@ -14,18 +16,36 @@ fn parse_input_string(input: &str) -> i32 {
             (first, second)
         })
         .unzip();
+    (v1, v2)
+}
+
+fn part1(input: &str) -> i32 {
+    let (mut v1, mut v2): (Vec<i32>, Vec<i32>) = parse_string(input);
     v1.sort_unstable();
     v2.sort_unstable();
 
     let sum = v1
-        .into_iter()
-        .zip(v2.into_iter())
+        .iter()
+        .zip(v2.iter())
         .map(|(a, b)| {
             let delta = (a - b).abs();
             delta
         })
         .sum();
     sum
+}
+
+fn parse_similarity_score(input: &str) -> i32 {
+    let (v1, v2): (Vec<i32>, Vec<i32>) = parse_string(input);
+    let mut counter = BTreeMap::new();
+    v2.into_iter().for_each(|x| {
+        // https://doc.rust-lang.org/std/collections/btree_map/struct.BTreeMap.html#method.entry
+        counter.entry(x).and_modify(|curr| *curr += 1).or_insert(1);
+    });
+
+    v1.iter().map(|x| {
+        counter.get(x).unwrap_or(&0) * x
+    }).sum()
 }
 
 #[cfg(test)]
@@ -39,16 +59,30 @@ mod tests {
 3   3";
 
     #[test]
-    fn test_small_sample() {
+    fn test_part1_small_sample() {
         let input = SAMPLE_INPUT;
-        let result = parse_input_string(input);
+        let result = part1(input);
         assert_eq!(result, 11);
     }
 
     #[test]
-    fn test_parse_str() {
+    fn test_part1_real_problem() {
         let input = include_str!("input.txt");
-        let result = parse_input_string(input);
+        let result = part1(input);
         assert_eq!(result, 1530215);
+    }
+
+    #[test]
+    fn test_part2_small_sample() {
+        let input = SAMPLE_INPUT;
+        let result = parse_similarity_score(input);
+        assert_eq!(result, 31);
+    }
+
+    #[test]
+    fn test_part2_real_problem() {
+        let input = include_str!("input.txt");
+        let result = parse_similarity_score(input);
+        assert_eq!(result, 26800609);
     }
 }
