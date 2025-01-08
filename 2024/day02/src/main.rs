b0889f4bf220f1114b1752c092f5fb20e552b7b0
Junichi Uekawa <dancer@debian.org>
2025-01-08 12:49:50 +0900

    2 stuff.

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -1,5 +1,41 @@
-fn part1(_input: &str) -> i32 {
-    11
+#[derive(PartialEq)]
+enum Relative {
+    Positive,
+    Negative,
+    Invalid,
+}
+
+fn part1(input: &str) -> usize {
+    input.lines().filter(|line| {
+        let line = line
+            .split_ascii_whitespace()
+            .map(|x| x.parse::<i32>().expect("Need a number"))
+            .collect::<Vec<i32>>()
+            .windows(2)
+            .map(|arr| {
+                let x = arr.get(0).expect("Need a first number");
+                let y = arr
+                    .get(1)
+                    .expect("Need two numbers. windows should give me two numbers, why?");
+                let relative = match x - y {
+                    1..=3 => Relative::Positive,
+                    -3..=-1 => Relative::Negative,
+                    _ => Relative::Invalid,
+                };
+                relative
+            })
+            .collect::<Vec<_>>();
+        if line.len() < 2 {
+            panic!("I didn't think about what to do when there's less than 2 docs");
+        }
+        if !line.windows(2).all(|window| window[0] == window[1]) {
+            return false;
+        }
+        if line[0] == Relative::Invalid {
+            return false;
+        }
+        true
+    }).count()
 }
 
 #[cfg(test)]
@@ -17,6 +53,6 @@ mod tests {
     fn test_part1_small_sample() {
         let input = SAMPLE_INPUT;
         let result = part1(input);
-        assert_eq!(result, 11);
+        assert_eq!(result, 2);
     }
 }
